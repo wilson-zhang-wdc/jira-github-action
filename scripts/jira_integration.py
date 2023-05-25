f9193ec2ca2343d737c40105a591706112562095
Wilson Zhang <wilsonzhang6@gmail.com>
2023-05-25 10:03:35 -0700

    STAR-26942 update action

--- scripts/jira_integration.py
+++ scripts/jira_integration.py
@@ -13,6 +13,7 @@ else:
     print(rf"Expected 3 parameters, only {len(sys.argv)-1} were provided")
     sys.exit(1)
 
+print(os.environ(["GITHUB_CONTEXT"]))
 print(json.loads(commit_message))
 sys.exit(1)
 
