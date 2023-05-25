cac1d5e1b0e839f42d2621801db706b66134c0d9
Wilson Zhang <wilsonzhang6@gmail.com>
2023-05-25 10:27:48 -0700

    STAR-26942 update action

--- scripts/jira_integration.py
+++ scripts/jira_integration.py
@@ -13,7 +13,9 @@ else:
     print(rf"Expected 3 parameters, only {len(sys.argv)-1} were provided")
     sys.exit(1)
 
-print(github_data)
+print(os.getenv("GITHUB_HEAD_REF"))
+print(os.getenv("GITHUB_REF"))
+sys.exit(1)
 commit_message = github_data["event"]["head_commit"]["message"]
 
 # Extract jira key from commit message
